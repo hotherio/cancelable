71c1cba9966c10f737963e4eabf9e1b730eaccb6
Alexandre Quemy <alexandre@hother.io>
2025-11-10 20:50:58 +0100

    chore: remove static/docs from main branch
    The static/docs directory contains built documentation that should
    only exist in the gh-pages branch, not in main. Added to .gitignore
    to prevent accidental commits.

--- static/docs/javascripts/tablesort.js
+++
@@ -1,9 +0,0 @@
-/**
- * Initialize tablesort on all tables in the documentation
- */
-document$.subscribe(function() {
-  var tables = document.querySelectorAll("article table:not([class])")
-  tables.forEach(function(table) {
-    new Tablesort(table)
-  })
-})
